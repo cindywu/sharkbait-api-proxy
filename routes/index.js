3b09950ac4db90da78253f4ef3132172e0d203db
cindy <1177031+cindywu@users.noreply.github.com>
2023-03-04 10:27:39 -1000

    console log

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', async (req, res) => {
   console.log('hi')
   try {
     const params = url.parse(req.url, true).query
+    console.log(`${API_BASE_URL}${params.q}${API_OPTIONS_STRING}`)
     const apiRes = await needle('get', `${API_BASE_URL}${params.q}${API_OPTIONS_STRING}`)
     const data = apiRes.body
     res.status(200).json(data)
